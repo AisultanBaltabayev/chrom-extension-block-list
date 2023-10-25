e8d2eb1917c891ce5decbeed6243ea530b094269
a.baltabayev <a.baltabayev>
2023-10-25 16:17:50 +0600

    back: add global validater

--- server/src/auth/dto/dto.ts
+++ server/src/auth/dto/dto.ts
@@ -1,13 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class SignUpBodyDto {
   @ApiProperty({
     example: 'example@gmail.com',
   })
+  @IsEmail()
   email: string;
+
   @ApiProperty({
     example: 'pass1234',
   })
+  @IsNotEmpty()
   password: string;
 }
 
