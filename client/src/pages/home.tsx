dd86216c347c541ec8e678da26394b7488f42e68
a.baltabayev <a.baltabayev>
2023-11-04 02:14:01 +0600

    front: implement custom ui-kit(button, select, text-field, link, header, logo, spinners) && add classNames function similar to clsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,12 +1,16 @@
-import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import {
   authControllerGetSessionInfo,
   authControllerSignIn,
 } from "@/shared/api/orval/auth/auth";
-
-const inter = Inter({ subsets: ["latin"] });
+import { UiButton } from "@/shared/ui/ui-button";
+import { UiTextField } from "@/shared/ui/ui-text-field";
+import { UiSelectField } from "@/shared/ui/ui-select-field";
+import { UiLink } from "@/shared/ui/ui-link";
+import { UiSpinner } from "@/shared/ui/ui-spinner";
+// import { UiPageSpinner } from "@/shared/ui/ui-page-spinner";
+import { UiHeader } from "@/shared/ui/ui-header";
 
 export default function HomePage() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -30,10 +34,49 @@ export default function HomePage() {
   });
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      {data?.email}
+    <main className={"min-h-screen"}>
+      <UiHeader right={<div>{data?.email}</div>} />
+
+      <UiButton variant={"primary"}>1</UiButton>
+      <UiButton variant={"secondary"}>2</UiButton>
+      <UiButton variant={"outlined"}>3</UiButton>
+      <UiTextField
+        label={"Text field"}
+        inputProps={{ placeholder: "Enter email" }}
+      />
+      <UiTextField
+        error={"Invalid email"}
+        inputProps={{ placeholder: "Enter email" }}
+      />
+      <UiTextField />
+      <UiSelectField
+        label={"Text field"}
+        selectProps={{ placeholder: "Enter email" }}
+        options={[
+          { label: "1", value: "1" },
+          { label: "2", value: "2" },
+          { label: "3", value: "3" },
+        ]}
+      />
+      <UiSelectField
+        error={"Invalid email"}
+        selectProps={{ placeholder: "Enter email" }}
+        options={[
+          { label: "1", value: "1" },
+          { label: "2", value: "2" },
+          { label: "3", value: "3" },
+        ]}
+      />
+      <UiSelectField
+        options={[
+          { label: "1", value: "1" },
+          { label: "2", value: "2" },
+          { label: "3", value: "3" },
+        ]}
+      />
+      <UiLink href={"/"}>asdasd</UiLink>
+      <UiSpinner className={"text-teal-600 w-20 h-20"} />
+      {/*<UiPageSpinner />*/}
     </main>
   );
 }
--- client/src/shared/lib/classNames.ts
+++ client/src/shared/lib/classNames.ts
@@ -1,8 +1,9 @@
+type Additional = (string | undefined)[];
 type Mods = Record<string, boolean | string>;
 
 export function classNames(
   cls: string,
-  additional: string[] = [],
+  additional: Additional = [],
   mods: Mods = {},
 ): string {
   const modsClassNames = Object.keys(mods).filter(
